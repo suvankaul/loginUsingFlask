5f44f5682c16af2500304b02f307ca0bd07bc3de
Suvan Kaul <suvankaul97@gmail.com>
2019-07-09 10:58:08 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # Import files
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, session, abort
 from flaskext.mysql import MySQL
+from flask import Markup
+import os
 
 # Creating instance of MySQL package
 mysql = MySQL()
 # Creating instance for flask
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
 # Mysql information
 app.config['MYSQL_DATABASE_USER'] = 'phpmyadminuser'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
@@ -16,7 +19,12 @@ mysql.init_app(app)
 
 @app.route('/')
 def my_form():
-    return render_template('form_ex.html')
+    if not session.get('logged_in'):
+        return render_template('form_ex.html')
+    else:
+        #message = Markup('"Hello Boss!  <a href="/logout">Logout</a>"')
+        #flash(message)
+        return '<html><body>Hello Boss!  <a href="/logout">Logout</a></body></html>'
 
 @app.route('/', methods = ['POST'])
 def Authenticate():
@@ -28,9 +36,16 @@ def Authenticate():
     cursor.execute(stmt,data)
     data = cursor.fetchone()
     if data is None:
-        return "Username or password is wrong"
+        flash("Username or password is wrong")
+        return my_form()
     else:
-        return "Logged in Sucessfully"
+        session['logged_in'] = True
+        return my_form()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return my_form()
 
 if __name__== "__main__":
     app.run()
